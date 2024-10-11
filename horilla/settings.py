82a9ea7ec91b456f3f2a46024c4876070febab38
NATURAVELO <114339432+bastien-naturavelo@users.noreply.github.com>
2024-10-11 14:13:06 +0200

    Update settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -44,8 +44,7 @@ DEBUG = env("DEBUG", False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS",["*"])
 
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS",["http://localhost:8000"])
-print(env.str('CSRF_TRUSTED_ORIGINS', multiline=True))
-print(env.str('SECRET_KEY', multiline=True))
+
 # Application definition
 
 INSTALLED_APPS = [
